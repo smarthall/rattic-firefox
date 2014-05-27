b9712fc2cb4f7b3deafa761f5566f54cf3f2cf7c
Daniel Hall <daniel.hall@rea-group.com>
2014-05-27 11:38:05 +1000

    Injecting a link on relavent pages

--- data/injection.js
+++ data/injection.js
@@ -1,4 +1,35 @@
-// This script gets injected into each page that RatticDB recognises.
+// This script gets injected into each page that RatticDB
+// recognises.
 
-document.body.style.border = "5px solid red"
+function findPasswordBoxes() {
+    var pwdinput = [];
+    var inputs = document.getElementsByTagName("INPUT");
+    for (var x = 0; x < inputs.length; x++) {
+        if (inputs[x].type == "password") {
+            pwdinput.push(inputs[x]);
+        }
+    }
+
+    return pwdinput;
+}
+
+document.body.style.border = "5px solid red";
+var boxes = findPasswordBoxes();
+for (var x = 0; x < boxes.length; x++) {
+    var input = boxes[x],
+        nparent = input.parentElement,
+        link = document.createElement('span');
+
+    // Build our link
+    link.innerHTML = "Rattic";
+    link.addEventListener("click", _ratticFillClick);
+
+    // Inject the link
+    input.style.border="5px solid red";
+    nparent.insertBefore(link, input.nextSibling);
+}
+
+function _ratticFillClick() {
+    alert("Clicked!");
+}
 
